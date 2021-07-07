82a823dd961bdb4bfcf26b73454fa2959c0869ca
mulicsanin <mulic.sanin@gmail.com>
2021-07-07 10:41:56 +0200

    .

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,21 +6,21 @@ import Logo from '../public/images/Logo.png';
 
 const handleBurger = () => {
 
-    // Get all "navbar-burger" elements
+   
     const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
   
-    // Check if there are any navbar burgers
+ 
     if ($navbarBurgers.length > 0) {
   
-      // Add a click event on each of them
+      
       $navbarBurgers.forEach( el => {
         el.addEventListener('click', () => {
   
-          // Get the target from the "data-target" attribute
+         
           const target = el.dataset.target;
           const $target = document.getElementById(target);
   
-          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+          
           el.classList.toggle('is-active');
           $target.classList.toggle('is-active');
   
@@ -40,7 +40,7 @@ export default class Navbar extends Component {
                                 <Image src={Logo} alt="LOGO" width={170} height={68}/>
                             </a>
                         </Link>
-                        <a role="button" onClick={handleBurger} class="navbar-burger" data-target="navMenu" aria-label="menu" aria-expanded="false">
+                        <a role="button" onClick={handleBurger} className="navbar-burger" data-target="navMenu" aria-label="menu" aria-expanded="false">
                             <span aria-hidden="true"></span>
                             <span aria-hidden="true"></span>
                             <span aria-hidden="true"></span>
